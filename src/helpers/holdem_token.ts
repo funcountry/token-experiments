5b37dd0e7b75ce0da91ab5e68e138d4c6d0c80a4
aelaguiz <aelaguiz@gmail.com>
2022-05-29 13:30:20 -0500

    Yes transfer worked

--- src/helpers/holdem_token.ts
+++ src/helpers/holdem_token.ts
@@ -8,74 +8,71 @@ function load_key(key:any) {
     return solana.Keypair.fromSecretKey(Buffer.from(key));
 }
 
-async function transfer(tokenMintAddress: string, wallet: any, to: string, connection: any, amount: number) {
-    const mintPublicKey = new solana.PublicKey(tokenMintAddress);
-    console.log(splToken);
-    console.log(splToken.Token);
-    const mintToken = new splToken.Token(
+async function transfer(mint:any, kp: any, toAddress: string, connection: any, amount: number) {
+    // const mintPublicKey = new solana.PublicKey(tokenMintAddress);
+    // console.log(splToken);
+    // console.log(splToken.Token);
+    // const mintToken = new splToken.Token(
+    //     connection,
+    //     mintPublicKey,
+    //     splToken.TOKEN_PROGRAM_ID,
+    //     wallet.payer // the wallet owner will pay to transfer and to create recipients associated token account if it does not yet exist.
+    // );
+
+    const fromTokenAccount = await splToken.getOrCreateAssociatedTokenAccount(
         connection,
-        mintPublicKey,
-        splToken.TOKEN_PROGRAM_ID,
-        wallet.payer // the wallet owner will pay to transfer and to create recipients associated token account if it does not yet exist.
+        kp,
+        mint.address,
+        kp.publicKey);
+    console.log("From Token Account");
+    console.log(fromTokenAccount.address.toString());
+
+    const toWallet = new solana.PublicKey(toAddress);
+
+    const toTokenAccount = await splToken.getOrCreateAssociatedTokenAccount(
+        connection,
+        kp,
+        mint.address,
+        toWallet);
+    console.log("To Token Account");
+    console.log(toTokenAccount.address.toString());
+
+    const res = await splToken.transfer(
+        connection,
+        kp,
+        fromTokenAccount.address,
+        toTokenAccount.address,
+        kp,
+        amount
     );
+    console.log(res);
 
-    const fromTokenAccount = await mintToken.getOrCreateAssociatedAccountInfo(
-        wallet.publicKey
-    );
+    // const receiverAccount = await splToken.getAccount(connection, toTokenAccount.address);
+    // console.log("Receiver account");
+    // console.log(receiverAccount.address.toString());
 
-    const destPublicKey = new solana.PublicKey(to);
 
-    // Get the derived address of the destination wallet which will hold the custom token
-    const associatedDestinationTokenAddr = await splToken.Token.getAssociatedTokenAddress(
-        mintToken.associatedProgramId,
-        mintToken.programId,
-        mintPublicKey,
-        destPublicKey
-    );
+//     instructions.push(
+//         splToken.Token.createTransferInstruction(
+//             splToken.TOKEN_PROGRAM_ID,
+//             fromTokenAccount.address,
+//             associatedDestinationTokenAddr,
+//             wallet.publicKey,
+//             [],
+//             amount
+//         )
+//     );
 
-    const receiverAccount = await connection.getAccountInfo(associatedDestinationTokenAddr);
+//     const transaction = new solana.Transaction().add(...instructions);
+//     transaction.feePayer = wallet.publicKey;
+//     transaction.recentBlockhash = (await connection.getRecentBlockhash()).blockhash;
 
-    // const instructions: solana.TransactionInstruction[] = [];
-    const instructions:Array<any> = [];
+//     const transactionSignature = await connection.sendRawTransaction(
+//         transaction.serialize(),
+//         { skipPreflight: true }
+//     );
 
-    // instructions = new Array<solana.TransactionInstruction>();
-
-    if (receiverAccount === null) {
-
-        instructions.push(
-            splToken.Token.createAssociatedTokenAccountInstruction(
-                mintToken.associatedProgramId,
-                mintToken.programId,
-                mintPublicKey,
-                associatedDestinationTokenAddr,
-                destPublicKey,
-                wallet.publicKey
-            )
-        )
-
-    }
-
-    instructions.push(
-        splToken.Token.createTransferInstruction(
-            splToken.TOKEN_PROGRAM_ID,
-            fromTokenAccount.address,
-            associatedDestinationTokenAddr,
-            wallet.publicKey,
-            [],
-            amount
-        )
-    );
-
-    const transaction = new solana.Transaction().add(...instructions);
-    transaction.feePayer = wallet.publicKey;
-    transaction.recentBlockhash = (await connection.getRecentBlockhash()).blockhash;
-
-    const transactionSignature = await connection.sendRawTransaction(
-        transaction.serialize(),
-        { skipPreflight: true }
-    );
-
-    await connection.confirmTransaction(transactionSignature);
+//     await connection.confirmTransaction(transactionSignature);
 }
 
 
@@ -104,11 +101,11 @@ export class HoldemTokenManager {
         console.log("Got mint", this.mint);
     }
 
-    public async grant(to_address:string, amount:number) {
+    public async grant(toAddress:string, amount:number) {
         console.log(this.mint);
-        console.log("Granting", amount, "tokens to", to_address);
+        console.log("Granting", amount, "tokens to", toAddress);
 
-        await transfer(this.mint.address, this.wallet, to_address,
+        await transfer(this.mint, this.kp, toAddress,
             this.connection, amount);
     }
 }
