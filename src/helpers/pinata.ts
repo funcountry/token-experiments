b550dbfc16b8fd2c5b8e9ae13bc781088a328412
aelaguiz <aelaguiz@gmail.com>
2022-05-31 20:34:58 -0500

    IPFS uploading sorta works now

--- src/helpers/pinata.ts
+++ src/helpers/pinata.ts
@@ -1,10 +1,11 @@
-import * as node_fetch from 'node-fetch';
+import FormData from 'form-data';
+// import node_fetch from 'node-fetch';
+import fetch from 'node-fetch';
 import fs from 'fs';
 
 
 // import log from 'loglevel';
 // import fetch from 'node-fetch';
-// import FormData from 'form-data';
 // import fs from 'fs';
 // import { setImageUrlManifest } from './file-uri';
 
@@ -14,10 +15,12 @@ import fs from 'fs';
 // }
 
 async function uploadMedia(media:string, jwt:string) {
+    console.log(media);
     const data = new FormData();
     data.append('file', fs.createReadStream(media));
+    console.log(data);
 
-    const res = await node_fetch(`https://api.pinata.cloud/pinning/pinFileToIPFS`, {
+    const res = await fetch(`https://api.pinata.cloud/pinning/pinFileToIPFS`, {
         headers: {
             Authorization: `Bearer ${jwt}`,
         },
@@ -26,6 +29,8 @@ async function uploadMedia(media:string, jwt:string) {
     });
 
     const json:any = await res.json();
+    console.log(jwt);
+    console.log(json);
     return json.IpfsHash;
 }
 
@@ -40,34 +45,34 @@ export async function pinataUpload(
 
     const imageCid = await uploadMedia(image, jwt);
     console.log('uploaded image: ', `${gatewayUrl}/ipfs/${imageCid}`);
-    await sleep(500);
+    // await sleep(500);
 
-    let animationCid:any = undefined;
-    let animationUrl:any = undefined;
-    if (animation) {
-        animationCid = await uploadMedia(animation, jwt);
-        console.log('uploaded image: ', `${gatewayUrl}/ipfs/${animationCid}`);
-    }
+    // let animationCid:any = undefined;
+    // let animationUrl:any = undefined;
+    // if (animation) {
+    //     animationCid = await uploadMedia(animation, jwt);
+    //     console.log('uploaded image: ', `${gatewayUrl}/ipfs/${animationCid}`);
+    // }
 
-    const mediaUrl = `${gatewayUrl}/ipfs/${imageCid}`;
-    if (animationCid) {
-        animationUrl = `${gatewayUrl}/ipfs/${animationCid}`;
-    }
+    // const mediaUrl = `${gatewayUrl}/ipfs/${imageCid}`;
+    // if (animationCid) {
+    //     animationUrl = `${gatewayUrl}/ipfs/${animationCid}`;
+    // }
 
-    const manifestJson = await setImageUrlManifest(
-        manifestBuffer.toString('utf8'),
-        mediaUrl,
-        animationUrl,
-    );
+    // const manifestJson = await setImageUrlManifest(
+    //     manifestBuffer.toString('utf8'),
+    //     mediaUrl,
+    //     animationUrl,
+    // );
 
-    fs.writeFileSync('tempJson.json', JSON.stringify(manifestJson));
+    // fs.writeFileSync('tempJson.json', JSON.stringify(manifestJson));
 
-    const metadataCid = await uploadMedia('tempJson.json', jwt);
+    // const metadataCid = await uploadMedia('tempJson.json', jwt);
 
-    await sleep(500);
+    // await sleep(500);
 
-    const link = `${gatewayUrl}/ipfs/${metadataCid}`;
-    console.log('uploaded manifest: ', link);
+    // const link = `${gatewayUrl}/ipfs/${metadataCid}`;
+    // console.log('uploaded manifest: ', link);
 
-    return [link, mediaUrl, animationUrl];
+    // return [link, mediaUrl, animationUrl];
 }
--- src/pinata-test.ts
+++ src/pinata-test.ts
@@ -1,3 +1,16 @@
 import * as pinata from './helpers/pinata';
+const config = require('../config.json');
 
 console.log(pinata);
+
+const run = async() => {
+    await pinata.pinataUpload(
+        "./nfts/citizen_placements_v1/assets/0.gif",
+        "",
+        Buffer.from(JSON.stringify("{}")),
+        config.pinataJwt,
+        null
+    );
+};
+
+run();
