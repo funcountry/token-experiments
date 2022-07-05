c3e8f2ef71102c924a0667857a7e5d7a34000cd4
aelaguiz <aelaguiz@gmail.com>
2022-07-04 22:52:43 -0500

    WIP

---
+++ src/hand_achievements.ts
@@ -0,0 +1,12 @@
+/*
+ * export function handRank(score: number): HandRanks {
+    if (score > 6185) return 'high_card'; // 1277 high card
+    if (score > 3325) return 'one_pair'; // 2860 one pair
+    if (score > 2467) return 'two_pair'; //  858 two pair
+    if (score > 1609) return 'three_of_a_kind'; //  858 three-kind
+    if (score > 1599) return 'straight'; //   10 straights
+    if (score > 322) return 'flush'; // 1277 flushes
+    if (score > 166) return 'full_house'; //  156 full house
+    if (score > 10) return 'four_of_a_kind'; //  156 four-kind
+    return 'straight_flush'; //   10 straight-flushes
+}*/
